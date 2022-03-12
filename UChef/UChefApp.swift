245873c97832dd9f36ab0ddbc176595f6a4ee4fd
Daniel Li <ad841108@yahoo.com.tw>
2022-03-12 14:29:23 -0500

    Initial Commit

---
+++ UChef/UChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  UChefApp.swift
+//  UChef
+//
+//  Created by Daniel Li on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct UChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
